088402a25937c0d17fb01300e3b6f3461cde7ca8
Roman Kozin <enatik14@gmail.com>
2024-12-04 12:30:05 +0300

    slows down for some reason...

--- chapter 5/5_12.py
+++ chapter 5/5_12.py
@@ -3,25 +3,30 @@ import numpy as np
 import os
 import time
 import matplotlib.pyplot as plt
+import random
+import gc
 
 def argMaxLastNAxes(arr, N):
     s = arr.shape
     new_shp = s[:-N] + (np.prod(s[-N:]),)
     max_idx = arr.reshape(new_shp).argmax(-1)
-    return np.unravel_index(max_idx, s[-N:])
+    return np.unravel_index(max_idx, s[-N:])[0]
 
 class RaceTrack:
-    def __init__(self, episodeDuration = 20, velocityLimit = (5, 5), stepReward = -1) -> None:
+    def __init__(self, episodeDuration = 40, velocityLimit = (5, 5), stepReward = -1, gamma = 1) -> None:
         self.createTrack()
         self.T = episodeDuration
         self.xVelRange = [-1, 1]
         self.yVelRange = [-1, 1]
-        self.Q = np.zeros((*self.track.shape, self.xVelRange[-1] - self.xVelRange[0] + 1, self.yVelRange[-1] - self.yVelRange[0] + 1))
+        self.Q = np.zeros((*self.track.shape, (self.xVelRange[-1] - self.xVelRange[0] + 1) * (self.yVelRange[-1] - self.yVelRange[0] + 1)))
+        self.evaluatePolicy()
         self.vel = [0, 0]
         self.velocityLimit = velocityLimit
         self.stepReward = stepReward
         self.count = np.zeros_like(self.Q)
         self.pos = [0, 0]
+        self.gamma = gamma
+        self.episodeNum = 0
         
     def createTrack(self):
         track = np.zeros((32,17), dtype=np.int8)
@@ -39,10 +44,24 @@ class RaceTrack:
         self.finishLine = [[0, 16], [5, 16]]
         
     def evaluatePolicy(self):
-        self.policy = argMaxLastNAxes(self.Q, 2)
+        self.policy = np.argmax(self.Q, axis=-1)
     
     def offPolicyMC(self):
-        states, actions, rewards = self.sampleData()
+        while True:
+            self.episodeNum += 1
+            states, actions, rewards = self.sampleData()
+            b = 1/9
+            G = 0
+            W = 1
+            for t in range(len(states) - 1, -1, -1):
+                G = self.gamma * G + rewards[t]
+                self.count[*states[t], actions[t]] += W
+                self.Q[*states[t], actions[t]] += W / self.count[*states[t], actions[t]] * (G - self.Q[*states[t], actions[t]])
+                self.evaluatePolicy()
+                if actions[t] != self.policy[*states[t]]:
+                    break
+                W /= b
+
     
     
     def sampleData(self):
@@ -55,10 +74,10 @@ class RaceTrack:
         
         for _ in range(self.T):
             states.append(self.pos)
-            # self.draw()
+            self.draw()
             xVelInc, yVelInc = self.sampleAction()
                 
-            actions.append([xVelInc + 1, yVelInc + 1])
+            actions.append((xVelInc + 1) * 3 + (yVelInc + 1))
                 
             self.vel[0] += xVelInc
             self.vel[1] += yVelInc
@@ -81,6 +100,8 @@ class RaceTrack:
             
             self.pos[0] += self.vel[0]
             self.pos[1] += self.vel[1]
+        gc.collect()
+        return states, actions, rewards
         
     def sampleStartingPoint(self):
         self.pos = [np.random.randint(self.startingLine[0][0], self.startingLine[1][0]+1), np.random.randint(self.startingLine[0][1], self.startingLine[1][1]+1)]
@@ -93,8 +114,14 @@ class RaceTrack:
         velNorm = np.linalg.norm([newXVel, newYVel])
         firstEntrance = True
         while not (0 < velNorm <= np.sqrt(2 * 16)) or not (-5 < newXVel <= 0) or not (0 <= newYVel < 5) or newXVel * newYVel == 0 or firstEntrance:
-            xVelInc = np.random.randint(self.xVelRange[0], self.xVelRange[1] + 1)
-            yVelInc = np.random.randint(self.yVelRange[0], self.yVelRange[1] + 1)
+            # xVelInc = np.random.randint(self.xVelRange[0], self.xVelRange[1] + 1)
+            # yVelInc = np.random.randint(self.yVelRange[0], self.yVelRange[1] + 1)
+            
+            xVelInc = random.randint(self.xVelRange[0], self.xVelRange[1])
+            yVelInc = random.randint(self.yVelRange[0], self.yVelRange[1])
+            
+            # xVelInc = int((self.xVelRange[1] + 1 - self.xVelRange[0])*random.random() +self.xVelRange[0] )
+            # yVelInc = int((self.yVelRange[1] + 1 - self.yVelRange[0])*random.random() +self.yVelRange[0] )
             
             newXVel = self.vel[0] + xVelInc
             newYVel = self.vel[1] + yVelInc
@@ -126,8 +153,9 @@ class RaceTrack:
         img = self.track.copy()
         img[self.pos[0], self.pos[1]] = 8
         plt.imshow(img)
-        plt.pause(1)        
+        plt.title(f"Episode num: {self.episodeNum}")
+        plt.pause(0.001)        
         
 exp = RaceTrack()
 plt.ion()
-exp.sampleData()
+exp.offPolicyMC()
